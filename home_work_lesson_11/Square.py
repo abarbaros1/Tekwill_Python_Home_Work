54f9907d93242724352904bf5e5743fe3197f638
Anatoli Barbaros <samtab.248496@gmail.com>
2022-03-03 19:30:47 +0200

    commit

--- home_work_lesson_11/Square.py
+++ home_work_lesson_11/Square.py
@@ -3,24 +3,16 @@ from Rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self,_inner_color,_border_color, _length):
         super().__init__(_inner_color,_border_color,_length, _length)
-        self._width = _length
-        self._length = _length
 
     def set_width(self,_width):
         self._width = _width
         self._length = _width
         # return self
 
-    def get_width(self):
-        return self._width
-
     def set_length(self,_length):
         self._length = _length
         self._width = _length
         # return self
-
-    def get_length(self):
-        return self._length
 
     def __str__(self):
         return f"{type(self).__name__} has width {self._width} and length {self._length}, with inner color {self._inner_color} and border color {self._border_color} "
