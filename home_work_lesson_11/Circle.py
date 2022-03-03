caff70d3452805a98004741379ed189b3d1f32d6
Anatoli Barbaros <samtab.248496@gmail.com>
2022-03-03 09:57:18 +0200

    commit

--- home_work_lesson_11/Circle.py
+++ home_work_lesson_11/Circle.py
@@ -1,8 +1,13 @@
 from Shape import Shape
-class Circle:
+
+class Circle(Shape):
+
     def __init__(self, inner_color,border_color,radius):
         super().__init__(inner_color,border_color)
         self.radius = radius
 
-a= Circle('red','green',5)
-print(a)
+    def __str__(self):
+        return f"{type(self).__name__} has radius of {self.radius} with inner color {self.inner_color.upper()} and with border color {self.border_color.upper()} "
+
+# a= Circle('red','green',5)
+# print(a)
