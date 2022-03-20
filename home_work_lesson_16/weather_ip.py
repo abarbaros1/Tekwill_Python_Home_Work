6e0970c7c378ad846b58d04b25b221301816b2d6
Anatoli Barbaros <samtab.248496@gmail.com>
2022-03-20 21:05:42 +0200

    commit

---
+++ home_work_lesson_16/weather_ip.py
@@ -0,0 +1,49 @@
+import requests
+import xmltodict,json
+
+
+
+def get_location(ip_data):
+    ip = ip_data
+
+    headers = {
+        'x-rapidapi-host': "ip-geolocation-ipwhois-io.p.rapidapi.com",
+        'x-rapidapi-key': "REMOVED"
+    }
+
+    response_l = requests.request("GET", ip, headers=headers).text
+
+    json_object = json.loads(response_l)
+    country = json_object['country']
+    city = json_object['city']
+    country_code = json_object['country_code']
+    region = {'country': json_object['country'], 'city': json_object['city'], 'country_code': json_object['country_code'].lower()}
+    return region
+
+# print(get_location('https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/'))
+
+def get_weather():
+    ip_data = "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+    weather_region = get_location(ip_data)
+
+    url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+    querystring = {"q": f"{weather_region['city']}, {weather_region['country_code']}", "lat": "0", "lon": "0",
+                   "callback": "", "id": "2172797", "lang": "null",
+                   "units": "metric", "mode": ""}
+
+    headers = {
+        'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+        'x-rapidapi-key': "REMOVED"
+    }
+
+    response_w = requests.request("GET", url, headers=headers, params=querystring).text
+    json_object_w = json.loads(response_w)
+
+    #print(json_object_w)
+    print('Temperature (celsius): ', json_object_w['main']['temp'])
+    print('Weather:', json_object_w['weather'][0]['main'])
+    print(weather_region['city'], ' ', weather_region['country'])
+
+get_weather()
+
